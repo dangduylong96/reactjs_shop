cac5fadbb12dd451f30bcdec95958ad6da9841b5
Duy Long <dangduylong96@gmail.com>
2018-06-05 09:52:07 +0700

    Chuan bi thu nghiem firebase

--- backend_shop_react/src/page/login.js
+++ backend_shop_react/src/page/login.js
@@ -2,36 +2,55 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            txtusername: '',
+            txtpassword: ''
+        }
+    }
+    handleInputChange=(event)=>{
+        let target=event.target;
+        let name=target.name;
+        let value=target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log(this.state);
+    }
     render() {
         return (
             <div className="hold-transition login-page">
                 <div className="login-box">
                     <div className="login-logo">
-                        <a href="../../index2.html"><b>Trang quản trị</b></a>
+                        <a href="javascript:void(0)"><b>Trang quản trị</b></a>
                     </div>
                     <div className="login-box-body">
                         <p className="login-box-msg">Nhập thông tin đăng nhập</p>
-                        <form action="../../index2.html" method="post">
-                        <div className="form-group has-feedback">
-                            <input type="text" className="form-control" placeholder="Tên tài khoản" />
-                            <span className="glyphicon glyphicon-envelope form-control-feedback" />
-                        </div>
-                        <div className="form-group has-feedback">
-                            <input type="password" className="form-control" placeholder="Password" />
-                            <span className="glyphicon glyphicon-lock form-control-feedback" />
-                        </div>
-                        <div className="row">
-                            <div className="col-xs-8">
-                            <div className="checkbox icheck">
-                                <label>
-                                <input type="checkbox" /> Nhớ tài khoản
-                                </label>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group has-feedback">
+                                <input name="txtusername" type="text" value={this.state.txtusername} onChange={this.handleInputChange} className="form-control" placeholder="Tên tài khoản" />
+                                <span className="glyphicon glyphicon-envelope form-control-feedback" />
                             </div>
+                            <div className="form-group has-feedback">
+                                <input name="txtpassword" type="password" value={this.state.txtpassword} onChange={this.handleInputChange} className="form-control" placeholder="Password" />
+                                <span className="glyphicon glyphicon-lock form-control-feedback" />
                             </div>
-                            <div className="col-xs-4">
-                            <button type="submit" className="btn btn-primary btn-block btn-flat">Đăng nhập</button>
+                            <div className="row">
+                                <div className="col-xs-8">
+                                    <div className="checkbox icheck">
+                                        <label>
+                                        <input type="checkbox" /> Nhớ tài khoản
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="col-xs-4">
+                                    <button type="submit" className="btn btn-primary btn-block btn-flat">Đăng nhập</button>
+                                </div>
                             </div>
-                        </div>
                         </form>
                     </div>
                 </div>
